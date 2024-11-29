e9141b0aba04d733c9a2a159e2ad7c06f9930f52
nawafrafif <yahyakhansa92@gmail.com>
2024-11-29 13:39:49 +0700

    pertemuan9

--- pertemuan9/p9.py
+++ pertemuan9/p9.py
@@ -1,27 +1,30 @@
-def celsius_ke_fahrenheit(celcius):
-    fahrenheit = (celcius * 9/5) + 32
-    print(fahrenheit)
+# print("=====SOAL NO1=====")
+# def celsius_ke_fahrenheit(celcius):
+#     fahrenheit = (celcius * 9/5) + 32
+#     return fahrenheit
     
-celsius_ke_fahrenheit(0)
-celsius_ke_fahrenheit(100)
+# print(celsius_ke_fahrenheit(0))
+# print(celsius_ke_fahrenheit(100))
+# print("=====SOAL NO2=====")
+# def is_genap(bilangan):
+#     hasil = bilangan % 2 == 0
 
-def is_genap(bilangan):
+#     return hasil
 
-    return bilangan % 2 == 0
+# print(is_genap(4))
+# print(is_genap(7))
+# print("=====SOAL NO3=====")
+# def cek_lulus(nilai):
+#     if nilai >= 70:
+#         return "lulus"
 
-print(is_genap(4))
-print(is_genap(7))
+#     else:
+#         return "gagal"
 
-def cek_lulus(nilai):
-    if nilai >= 70:
-        return ("lulus")
+# print(cek_lulus(80))
+# print(cek_lulus(60))
+# print("=====SOAL NO4=====")
+# def bilangan_ganjil(n):
+#     return[i for i in range(1, n+1) if i % 2 != 0]
+# print(bilangan_ganjil(20))
 
-    else:
-        return ("gagal")
-
-print(cek_lulus(80))
-print(cek_lulus(60))
-
-def bilangan_ganjil(n):
-    return[i for i in range(1, n+1) if i % 2 != 0]
-print(bilangan_ganjil(20))
